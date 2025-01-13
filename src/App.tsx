0c58257d9a172028c7e430ccc91b4df03d91247a
leeegyubin <sdhg12@naver.com>
2025-01-13 21:59:31 +0900

    feat: 캘린더 css 파일 생성

---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <div className="header">
+        <div>ㄱ</div>
+        <div>2025년</div>
+        <div>버튼1</div>
+        <div>버튼2</div>
+        <div>버튼3</div>
+      </div>
+
+      <div className="monthBox">
+        <div>
+        1월
+        </div>
+      </div>
+
+      <div className='weekName'>
+        <div>일</div>
+        <div>월</div>
+        <div>화</div>
+        <div>수</div>
+        <div>목</div>
+        <div>금</div>
+        <div>토</div>
+      </div>
+    <div className = 'day'>
+      <div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+      </div>
+
+      <div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+      </div>
+
+      <div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+      </div>
+
+      <div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+      </div>
+
+      <div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+      </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
